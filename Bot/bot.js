9c48373fb9b76d135c84e847327a31b9ba34a61e
Mixxar-One-Designer <muazzammixxa32@gmail.com>
2024-09-03 04:21:28 +0100

    Update Firebase setup for user data savings

--- Bot/bot.js
+++ Bot/bot.js
@@ -5,29 +5,26 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const web_link = "https://7d029740d8ae.ngrok.app";
-const community_link = "https://t.me/concept_developer";
+const web_link = "https://frogs-nu.vercel.app/";
+const community_link = "https://t.me/frogs_world";
+const image_url = "https://i.ibb.co/YbKjmHN/frogs.jpg"; // Updated with direct image URL
 
 bot.start((ctx) => {
     const startPayload = ctx.startPayload;
     const urlSent = `${web_link}?ref=${startPayload}`;
     const user = ctx.message.from;
     const userName = user.username ? `@${user.username}` : user.first_name;
-    ctx.replyWithMarkdown(`*Hey ${userName}, Welcome to My Bot! Contact the Developer of this bot web app @concept_developer for your own projects today*
-Tap on the coin and see token rise.
-      
-*THIS BOT Bot* is a Decentralized Exchange on the TON Blockchain. The biggest part of bot TOKEN distribution will occur among the players here.
-      
-Got friends, relatives, co-workers?
-Bring them all into the game.
-More buddies, more coins.`, {
+
+    // Send a single message with the image and text
+    ctx.replyWithPhoto(image_url, {
+        caption: `*Hey ${userName}, welcome to the Frogs Bot!* 🐸\n\nStart mining and collect $FROGS tokens as you dig deeper! Every tap brings you closer to becoming the top frog in the pond.\n\nInvite friends and climb the leaderboard together. More frogs, more fun, and more rewards!`,
+        parse_mode: 'Markdown',
         reply_markup: {
             inline_keyboard: [
-              [{ text: "👋 Start now!", web_app: { url: urlSent } }],
-              [{ text: "Join our Community", url: community_link }]
+                [{ text: "👋 Start Mining!", web_app: { url: urlSent } }],
+                [{ text: "Join our Community", url: community_link }]
             ],
-            in: true
-        },
+        }
     });
 });
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -10,12 +11,14 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
-
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 console.log("Firebase initialized:", app);
+
+// Initialize Firestore
+const db = getFirestore(app);
 console.log("Firestore initialized:", db);
 
 export { db };
